dd0e03f35193e11973931c9fe1405351a1d91693
fuad <fuadjemal@gmail.com>
2022-12-13 19:29:50 +0300

    fix(version updater) - include update_version.g.dart into git

---
+++ lib/models/update_version.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'update_version.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UpdateVersion _$UpdateVersionFromJson(Map<String, dynamic> json) =>
+    UpdateVersion()
+      ..version_number = json['version_number'] as String?
+      ..is_customer_app = json['is_customer_app'] as bool?
+      ..is_forceful_update = json['is_forceful_update'] as bool?
+      ..date_version_created =
+          FirebaseDocument.DateTimeFromJson(json['date_version_created']);
+
+Map<String, dynamic> _$UpdateVersionToJson(UpdateVersion instance) =>
+    <String, dynamic>{
+      'version_number': instance.version_number,
+      'is_customer_app': instance.is_customer_app,
+      'is_forceful_update': instance.is_forceful_update,
+      'date_version_created':
+          FirebaseDocument.DateTimeToJson(instance.date_version_created),
+    };
