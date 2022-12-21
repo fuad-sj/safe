f33b25a6b4aa7cee202aca2f6dcc7c8b795e6103
fuad <fuadjemal@gmail.com>
2022-12-21 22:26:16 +0300

    successful merge of payment_screen.dart and main_payment_screen.dart

--- lib/controller/main_payment_screen.dart
+++ lib/controller/main_payment_screen.dart
@@ -5,6 +5,12 @@ import 'package:flutter_gen/gen_l10n/safe_localizations.dart';
 import 'package:safe/controller/graphview/GraphView.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'dart:ui' as ui;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:safe/models/FIREBASE_PATHS.dart';
+import 'package:safe/models/customer.dart';
+import 'package:safe/models/referral_traversed_tree.dart';
+import 'package:safe/utils/pref_util.dart';
+import 'package:safe/controller/graphview/GraphView.dart';
 
 class homePage extends StatefulWidget {
   const homePage({Key? key}) : super(key: key);
@@ -13,41 +19,168 @@ class homePage extends StatefulWidget {
   _homePageState createState() => _homePageState();
 }
 
+class NodePair {
+  SubTreeNode treeNode;
+  Node graphNode;
+
+  NodePair(this.treeNode, this.graphNode);
+}
+
 class _homePageState extends State<homePage> {
   final Graph graph = Graph();
 
   SugiyamaConfiguration builder = SugiyamaConfiguration()
     ..bendPointShape = CurvedBendPointShape(curveLength: 20);
 
-  bool _isNodeClicked = false;
+  ReferralTraversedTree? _traversedTree;
+
+  NodePair? _rootNodeInfo = null; // useful for resetting the graph
+  Map<String, NodePair> _mapNodes = Map();
+  Map<String, Set<String>> _nodeDirectChildren = Map();
+  Set<String> _nodesNeedingReloadingChildren = Set();
+  bool graphLoadFinished = false;
+  Map<String, NodePair> _removedNodes = Map();
 
   @override
   void initState() {
     super.initState();
-    final node1 = Node.Id(1);
-    final node2 = Node.Id(2);
-    final node3 = Node.Id(3);
-    final node4 = Node.Id(4);
-    final node5 = Node.Id(5);
-    final node6 = Node.Id(6);
-    final node8 = Node.Id(7);
-    final node7 = Node.Id(8);
-    final node9 = Node.Id(9);
 
-    graph.addEdge(node1, node2, paint: Paint()..color = Colors.red);
-    graph.addEdge(node1, node3);
-    /*
-    graph.addEdge(node1, node4);
-    graph.addEdge(node2, node5);
-    graph.addEdge(node2, node6);
-    graph.addEdge(node3, node7);
-    graph.addEdge(node3, node8);
-    graph.addEdge(node5, node9);
-     */
     builder
       ..nodeSeparation = (15)
       ..levelSeparation = (15)
       ..orientation = SugiyamaConfiguration.ORIENTATION_TOP_BOTTOM;
+
+    loadTraversedTreeCache();
+  }
+
+  Future<void> loadTraversedTreeCache() async {
+    _traversedTree = ReferralTraversedTree.fromSnapshot(await FirebaseFirestore
+        .instance
+        .collection(FIRESTORE_PATHS.COL_REFERRAL_TRAVERSED_TREE)
+        .doc(PrefUtil.getCurrentUserID())
+        .get());
+
+    if (_traversedTree!.documentExists()) {
+      int len = (_traversedTree!.explored_nodes ?? []).length;
+      for (int i = 0; i < len; i++) {
+        SubTreeNode node = _traversedTree!.explored_nodes![i];
+        NodePair? updatedNodeInfo =
+            await loadUpdatedNodeInfo(node, node.node_id);
+        if (updatedNodeInfo == null) {
+          return;
+        }
+        _traversedTree!.explored_nodes![i] = updatedNodeInfo.treeNode;
+
+        _mapNodes[node.node_id] = updatedNodeInfo;
+        if (_rootNodeInfo == null) {
+          _rootNodeInfo = updatedNodeInfo;
+        }
+
+        SubTreeNode treeNode = updatedNodeInfo.treeNode;
+        // if the # of direct-children at the node has changed, load its children
+        if (treeNode.last_cached_num_direct_children !=
+            treeNode.updated_num_direct_children) {
+          _nodesNeedingReloadingChildren.add(treeNode.node_id);
+        }
+      }
+    } else {
+      // create a new traversal node if one doesn't exist, and put root node as the first node of list
+      _traversedTree = (new ReferralTraversedTree())
+        ..explored_links = []
+        ..explored_nodes = [];
+
+      _rootNodeInfo =
+          await loadUpdatedNodeInfo(null, PrefUtil.getCurrentUserID());
+      if (_rootNodeInfo == null) {
+        return; // if we can't have a root node, don't bother
+      }
+      _traversedTree!.explored_nodes!.add(_rootNodeInfo!.treeNode);
+    }
+
+    graph.addNode(_rootNodeInfo!.graphNode);
+
+    _traversedTree!.explored_links?.forEach((link) {
+      NodePair startNode = _mapNodes[link.start_node]!;
+      NodePair endNode = _mapNodes[link.end_node]!;
+
+      graph.addEdge(startNode.graphNode, endNode.graphNode);
+
+      if (!_nodeDirectChildren.containsKey(link.start_node)) {
+        _nodeDirectChildren[link.start_node] = new Set();
+      }
+
+      _nodeDirectChildren[link.start_node]!.add(link.end_node);
+    });
+
+    for (final nodeId in _nodesNeedingReloadingChildren) {
+      //await loadNodeDirectChildren(nodeId, false);
+    }
+
+    graphLoadFinished = true;
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<NodePair?> loadUpdatedNodeInfo(
+      SubTreeNode? prevInfo, String nodeId) async {
+    Customer customer = Customer.fromSnapshot(await FirebaseFirestore.instance
+        .collection(FIRESTORE_PATHS.COL_CUSTOMERS)
+        .doc("" + nodeId)
+        .get());
+    if (!customer.documentExists()) return null;
+
+    NodePair? nodePair;
+
+    if (_mapNodes.containsKey(nodeId)) {
+      nodePair = _mapNodes[nodeId]!;
+    } else {
+      nodePair = new NodePair(new SubTreeNode(nodeId), Node.Id(nodeId));
+      _mapNodes[nodeId] = nodePair;
+    }
+
+    SubTreeNode subNode = nodePair.treeNode;
+    subNode.updated_num_direct_children = customer.num_direct_children ?? 0;
+    subNode.updated_num_total_children = customer.num_total_children ?? 0;
+
+    if (prevInfo != null) {
+      subNode.last_cached_num_direct_children =
+          prevInfo.last_cached_num_direct_children;
+      subNode.last_cached_num_total_children =
+          prevInfo.last_cached_num_total_children;
+    }
+
+    return nodePair;
+  }
+
+  Future<void> saveTraversedTreeInfo() async {
+    // nothing to do if we don't have a tree
+    if (_traversedTree == null ||
+        (_traversedTree?.explored_nodes ?? []).length == 0) return;
+
+    _traversedTree!.explored_nodes!.forEach((node) {
+      node.last_cached_num_direct_children =
+          node.updated_num_direct_children ?? 0;
+      node.last_cached_num_total_children =
+          node.updated_num_total_children ?? 0;
+
+      node.updated_num_direct_children = null;
+      node.updated_num_total_children = null;
+    });
+
+    await FirebaseFirestore.instance
+        .collection(FIRESTORE_PATHS.COL_REFERRAL_TRAVERSED_TREE)
+        .doc(PrefUtil.getCurrentUserID())
+        .set(_traversedTree!.toJson(), SetOptions(merge: true));
+  }
+
+  @override
+  void dispose() {
+    Future.delayed(Duration.zero, () async {
+      await saveTraversedTreeInfo();
+    });
+    super.dispose();
   }
 
   List<_SalesData> data = [
@@ -413,7 +546,8 @@ class _homePageState extends State<homePage> {
                         ),
                       ),
                       Container(
-                        padding: EdgeInsets.only(left: hWidth * 0.03, right:  hWidth * 0.04 ),
+                        padding: EdgeInsets.only(
+                            left: hWidth * 0.03, right: hWidth * 0.04),
                         child: SfCartesianChart(
                           primaryXAxis: CategoryAxis(opposedPosition: false),
                           legend: Legend(isVisible: false),
@@ -464,7 +598,8 @@ class _homePageState extends State<homePage> {
                       ),
                       Container(
                         height: vHeight * 0.08,
-                        padding: EdgeInsets.only(left: hWidth * 0.03, right:  hWidth * 0.04 ),
+                        padding: EdgeInsets.only(
+                            left: hWidth * 0.03, right: hWidth * 0.04),
                         child: SfCartesianChart(
                           primaryXAxis: CategoryAxis(
                             majorGridLines: const MajorGridLines(width: 0),
@@ -485,61 +620,138 @@ class _homePageState extends State<homePage> {
                 )
               : Container(),
         ),
-        Expanded(
-          child: InteractiveViewer(
-              constrained: false,
-              boundaryMargin: EdgeInsets.all(100),
-              minScale: 0.0002,
-              maxScale: 10.6,
-              child: GraphView(
-                graph: graph,
-                algorithm: SugiyamaAlgorithm(builder),
-                paint: Paint()
-                  ..color = Colors.green
-                  ..strokeWidth = 1
-                  ..style = PaintingStyle.stroke,
-                builder: (Node node) {
-                  // I can decide what widget should be shown here based on the id
-                  var a = node.key!.value as int?;
-                  return rectangleWidget(a, () {
-                    if (!_isNodeClicked) {
-                      _isNodeClicked = true;
-                      var node1 = Node.Id(1);
-                      var node4 = Node.Id(4);
-                      graph.addEdge(node1, node4);
-                      setState(() {});
-                    } else {
-                      _isNodeClicked = false;
-                      var node1 = Node.Id(4);
-                      var node4 = Node.Id(4);
-                      graph.removeEdge(Edge(node1, node4));
-                      setState(() {});
-                    }
-                  });
-                },
-              )),
-        ),
+        if (graphLoadFinished) ...[
+          Expanded(
+            child: InteractiveViewer(
+                constrained: false,
+                boundaryMargin: EdgeInsets.all(100),
+                minScale: 0.0002,
+                maxScale: 10.6,
+                child: GraphView(
+                  graph: graph,
+                  algorithm: SugiyamaAlgorithm(builder),
+                  paint: Paint()
+                    ..color = Colors.green
+                    ..strokeWidth = 1
+                    ..style = PaintingStyle.stroke,
+                  builder: (Node node) {
+                    return nodeWidget(node.key!.value as String);
+                  },
+                )),
+          ),
+        ],
       ],
     );
   }
 
-  Random r = Random();
+  Widget nodeWidget(String node_id) {
+    NodePair nodePair = _mapNodes[node_id]!;
 
-  Widget rectangleWidget(int? a, VoidCallback callback) {
+    bool didTotalChildrenChange =
+        nodePair.treeNode.updated_num_total_children !=
+            nodePair.treeNode.last_cached_num_total_children;
+
     return GestureDetector(
-      onTap: callback,
-      child:  Container(
-          width: 100,
-          height: 50,
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            gradient: unSelectedDateColorGradiant(),
+      behavior: HitTestBehavior.opaque,
+      onTap: () async {
+        await loadNodeDirectChildren(node_id, true);
+        setState(() {});
+      },
+      child: Container(
+        width: 100,
+        height: 50,
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          gradient: unSelectedDateColorGradiant(),
+        ),
+        child: Center(
+          child: Text(
+            '${nodePair.treeNode.updated_num_total_children}',
+            style: selectedTextFieldStyle(),
           ),
-          child: Center(child: Text('Node $a',
-          style: selectedTextFieldStyle(),
-          ))),
+        ),
+      ),
     );
+  }
+
+  void removeSubTreeNode(String nodeId, bool removeSelf) {
+    // hack to remove ids from a map after iterating through them
+    // https://stackoverflow.com/a/22410848
+    var toRemoveId = [];
+    _nodeDirectChildren[nodeId]?.forEach((childId) {
+      removeSubTreeNode(childId, true);
+      toRemoveId.add(childId);
+    });
+
+    _nodeDirectChildren[nodeId]?.removeWhere((e) => toRemoveId.contains(e));
+
+    if (!removeSelf) return;
+
+    NodePair node = _mapNodes[nodeId]!;
+    graph.removeNode(node.graphNode);
+    _removedNodes[nodeId] = node;
+    _traversedTree!.explored_nodes!
+        .removeWhere((elem) => elem.node_id == nodeId);
+    _traversedTree!.explored_links!.removeWhere(
+        (link) => (link.start_node == nodeId) || (link.end_node == nodeId));
+  }
+
+  Future<void> loadNodeDirectChildren(
+      String parentId, bool clickInitiatedLoad) async {
+    if (_traversedTree == null ||
+        _rootNodeInfo ==
+            null) // we can't continue without actually having loaded the initial tree
+      return;
+
+    var directChildrenSnapshot = await FirebaseFirestore.instance
+        .collection(FIRESTORE_PATHS.COL_FLAT_REFERRAL_TREE)
+        .where(FlatAncestryNode.FIELD_PARENT_ID, isEqualTo: parentId)
+        .where(FlatAncestryNode.FIELD_SEPARATION, isEqualTo: 0)
+        .get();
+
+    if (directChildrenSnapshot.docs.isEmpty) {
+      return;
+    }
+
+    if (!_nodeDirectChildren.containsKey(parentId)) {
+      _nodeDirectChildren[parentId] = new Set();
+    }
+
+    // user is clicking again a node that has already been fully expanded
+    if (clickInitiatedLoad &&
+        (directChildrenSnapshot.docs.length ==
+            _nodeDirectChildren[parentId]!.length)) {
+      removeSubTreeNode(parentId, false);
+    } else {
+      for (final snapshot in directChildrenSnapshot.docs) {
+        FlatAncestryNode fNode = FlatAncestryNode.fromSnapshot(snapshot);
+
+        // if we've already seen this child, don't do anything
+        if (_nodeDirectChildren[parentId]!.contains(fNode.child_id!)) {
+          continue;
+        }
+        _nodeDirectChildren[parentId]!.add(fNode.child_id!);
+
+        NodePair? nodeInfo;
+        if (_removedNodes.containsKey(fNode.child_id!)) {
+          nodeInfo = _removedNodes[fNode.child_id!];
+          _removedNodes.remove(fNode.child_id!);
+        } else {
+          nodeInfo = await loadUpdatedNodeInfo(null, fNode.child_id!);
+        }
+        if (nodeInfo != null) {
+          _traversedTree!.explored_nodes!.add(nodeInfo.treeNode);
+          _traversedTree!.explored_links!
+              .add(SubTreeLink(parentId, fNode.child_id!));
+
+          Node source = _mapNodes[parentId]!.graphNode;
+          Node dest = nodeInfo.graphNode;
+
+          graph.addEdge(source, dest);
+        }
+      }
+    }
   }
 }
 
@@ -555,7 +767,6 @@ class DateOfEarning {
 
   final String date;
 }
-
 
 class CustomTotalPriceClipPath extends CustomClipper<Path> {
   var radius = 100.0;
