8e51e6b14a4674db02bc2d1cb2013fe5bdbba306
fuad <fuadjemal@gmail.com>
2023-04-20 14:57:39 +0300

    feat(additional shared ride fields) - add additional fields into shared rides broadcast

--- lib/models/shared_ride_broadcast.dart
+++ lib/models/shared_ride_broadcast.dart
@@ -5,6 +5,20 @@ class SharedRideBroadcast {
 
   late String place_name;
   late String place_id;
+  late List<double> place_loc;
+  late List<double> initial_loc;
+
+  late int created_timestamp;
+  int? ping_timestamp;
+  int? trip_started_timestamp;
+
+  String? driver_name;
+  String? driver_phone;
+  String? car_plate;
+  String? car_details;
+  bool? is_six_seater;
+
+  double? est_price;
 
   /**
    * TODO: do appropriate filtering of orders based on these
@@ -16,16 +30,8 @@ class SharedRideBroadcast {
   bool? is_trip_started;
   bool? is_trip_completed;
 
-  double? est_price;
-
-  bool? is_six_seater;
-
-  late int timestamp;
-
-  bool? is_cancelled;
-
-  late String car_plate;
-  late String car_details;
+  bool? is_forcefully_filled;
+  int? num_forceful_filled;
 
   int? seats_remaining;
 
@@ -47,19 +53,26 @@ class SharedRideBroadcast {
       ..ride_id = ride_id
       ..place_name = data["place_name"] ?? null
       ..place_id = data["place_id"] ?? null
+      ..place_loc = data["place_loc"] ?? null
+      ..initial_loc = data["initial_loc"] ?? null
+      ..created_timestamp = data["created_timestamp"] ?? null
+      ..ping_timestamp = data["ping_timestamp"] ?? null
+      ..trip_started_timestamp = data["trip_started_timestamp"] ?? null
+      ..driver_name = data["driver_name"] ?? null
+      ..driver_phone = data["driver_phone"] ?? null
+      ..car_plate = data["car_plate"] ?? null
+      ..car_details = data["car_details"] ?? null
+      ..is_six_seater = data["is_six_seater"] ?? null
+      ..est_price = data["est_price"] ?? null
       ..is_setup_done = data["is_setup_done"] ?? null
       ..is_trip_cancelled = data["is_trip_cancelled"] ?? null
       ..is_stale_order = data["is_stale_order"] ?? null
       ..is_fully_booked = data["is_fully_booked"] ?? null
       ..is_trip_started = data["is_trip_started"] ?? null
       ..is_trip_completed = data["is_trip_completed"] ?? null
-      ..est_price = data["est_price"] + 0.0 ?? null
-      ..is_six_seater = data["is_six_seater"] ?? null
-      ..timestamp = data["timestamp"] ?? null
-      ..is_cancelled = data["is_cancelled"] ?? null
-      ..seats_remaining = data["seats_remaining"] ?? null
-      ..car_plate = data["car_plate"] ?? null
-      ..car_details = data["car_details"] ?? null;
+      ..is_forcefully_filled = data["is_forcefully_filled"] ?? null
+      ..num_forceful_filled = data["num_forceful_filled"] ?? null
+      ..seats_remaining = data["seats_remaining"] ?? null;
   }
 }
 
